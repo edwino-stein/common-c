b131a470b6a823084352e870e806227fdc22d8de
Edwino Stein <edwino.stein@gmail.com>
2016-06-12 13:13:42 -0400

    Adicionando header common.h

---
+++ dev/common/common.h
@@ -0,0 +1,12 @@
+/**
+ *  Desenvolvido por Edwino Stein - edwino.stein@gmail.com
+ */
+
+#if !defined  _COMMON_BASE
+#define _COMMON_BASE
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdbool.h> //Define o tipo booleano
+
+#endif
